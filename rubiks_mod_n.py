8e3a2accc97a56572443044ccc8a82befdfa742b
Arhan Chaudhary <arhan.ch@gmail.com>
2024-12-11 07:02:30 +0000

    run steps 1-4

--- rubiks_mod_n.py
+++ rubiks_mod_n.py
@@ -1,135 +1,156 @@
-import math
-import copy
 import random
 
+
 def file_output(data_output, file_name):
-    with open(file_name,'w',encoding='utf-8') as f:
+    with open(file_name, "w", encoding="utf-8") as f:
         for l in data_output:
-           f.write(','.join([str(x) for x in l])+'\n')
+            f.write(",".join([str(x) for x in l]) + "\n")
+
 
 def file_input(file_name):
-
-    with open(file_name,encoding='utf-8',mode = 'r') as f:
+    with open(file_name, encoding="utf-8", mode="r") as f:
         lines = f.readlines()
-    data = [x.split(',') for x in lines]
-    #data.pop(0)
+    data = [x.split(",") for x in lines]
+    data.pop(0)
     for row in data:
-       row[-1] = row[-1][:-1]
+        row[-1] = row[-1][:-1]
     return data
 
-def gcd(a,b):
 
-    q = int(a/b)
+def gcd(a, b):
     r = a % b
-
     if r == 0:
         return b
+    return gcd(b, r)
 
-    return gcd(b,r)
 
 def random_alg(alg_len):
-    moves = ['R','L','U','D','F','B']
-    types = [' ',"' ",'2 ']
-    alg = ''
+    moves = ["R", "L", "U", "D", "F", "B"]
+    types = [" ", "' ", "2 "]
+    alg = ""
 
     randI = -1
     for move in range(alg_len):
         nextI = randI
         while nextI == randI:
-            nextI = int(random.random()*6)
+            nextI = int(random.random() * 6)
         randI = nextI
 
-        type = int(random.random()*3)
+        type = int(random.random() * 3)
         alg = alg + moves[randI] + types[type]
 
     return alg
 
-def insert_prime_powers(n,n_index,n_prime_powers,curr_alg,n_algs,curr_unit):
 
+def insert_prime_powers(n, n_index, n_prime_powers, curr_alg, n_algs, curr_unit):
     if n_index == len(n_prime_powers):
-
-        n_algs.append([curr_unit,curr_alg])
+        n_algs.append([curr_unit, curr_alg])
         return n_algs
-    
 
     for curr_multiplier in range(n_prime_powers[n_index][1]):
         if n_index == 0:
-            print("Main Cycle ",curr_multiplier," of ", len(n_prime_powers))
-        n_algs = insert_prime_powers(n,n_index+1,n_prime_powers,curr_alg + n_prime_powers[n_index][5][curr_multiplier],n_algs,curr_unit)
+            print("Main Cycle ", curr_multiplier, " of ", len(n_prime_powers))
+        n_algs = insert_prime_powers(
+            n,
+            n_index + 1,
+            n_prime_powers,
+            curr_alg + n_prime_powers[n_index][5][curr_multiplier],
+            n_algs,
+            curr_unit,
+        )
         curr_unit = (curr_unit * n_prime_powers[n_index][4]) % n
-    
+
     return n_algs
 
-def add_multipliers(n,n_prime_powers):
 
+def add_multipliers(n, n_prime_powers):
     for power in n_prime_powers:
-        #print(power)
+        # print(power)
         n_over = n // power[0]
         n_over_mult = n_over
         while (n_over_mult + 1) % power[0] != power[3]:
-            #print((n_over_mult + 1) % power[0],power[3])
-            #print(n_over_mult)
+            # print((n_over_mult + 1) % power[0],power[3])
+            # print(n_over_mult)
             n_over_mult += n_over
 
-        power.append(n_over_mult+1)
+        power.append(n_over_mult + 1)
         power.append([])
-        curr_alg = ''
+        curr_alg = ""
         for power_rep in range(power[1]):
             power[-1].append(curr_alg)
             curr_alg += power[2]
-            #curr_alg = optimize_alg(curr_alg)
-        
+            # curr_alg = optimize_alg(curr_alg)
+
     return n_prime_powers
 
-#def optimize_alg(alg_to_optimize):
 
-   # return random_alg(18)
+# def optimize_alg(alg_to_optimize):
 
-def compress(n_algs,symbol_index,new_symbols):
+# return random_alg(18)
 
+
+def compress(n_algs, symbol_index, new_symbols):
     freq_tracker = [0]
     b = len(symbol_index) + len(new_symbols)
 
     for unit in n_algs:
         unit[1] = [symbol_index.index(unit[1][x]) for x in range(len(unit[1]))]
-        for start_symbol in range(len(unit[1])-1):
-            freq_tracker = freq_insert(freq_tracker,unit[1][start_symbol],unit[1][start_symbol+1],b)
+        for start_symbol in range(len(unit[1]) - 1):
+            freq_tracker = freq_insert(
+                freq_tracker, unit[1][start_symbol], unit[1][start_symbol + 1], b
+            )
 
     compression = []
 
     for sym in new_symbols:
-        print('Compress',sym)
+        print("Compress", sym)
         max_index = freq_tracker.index(max(freq_tracker))
-        max_split = [max_index//b,max_index%b]
-        compression.append([sym,symbol_index[max_split[0]]+symbol_index[max_split[1]]])
+        max_split = [max_index // b, max_index % b]
+        compression.append(
+            [sym, symbol_index[max_split[0]] + symbol_index[max_split[1]]]
+        )
         for unit in n_algs:
-            for start_symbol in range(len(unit[1])-2,-1,-1):
-                if unit[1][start_symbol] == max_split[0] and unit[1][start_symbol+1] == max_split[1]:
-                    
-                    unit[1].pop(start_symbol+1)
+            for start_symbol in range(len(unit[1]) - 2, -1, -1):
+                if (
+                    unit[1][start_symbol] == max_split[0]
+                    and unit[1][start_symbol + 1] == max_split[1]
+                ):
+                    unit[1].pop(start_symbol + 1)
                     unit[1][start_symbol] = len(symbol_index)
 
                     if start_symbol > 0:
-                        freq_tracker[unit[1][start_symbol-1]*b+max_split[0]] -= 1
-                        freq_tracker = freq_insert(freq_tracker,unit[1][start_symbol-1],unit[1][start_symbol],b)
+                        freq_tracker[unit[1][start_symbol - 1] * b + max_split[0]] -= 1
+                        freq_tracker = freq_insert(
+                            freq_tracker,
+                            unit[1][start_symbol - 1],
+                            unit[1][start_symbol],
+                            b,
+                        )
 
                     if start_symbol + 1 < len(unit[1]):
-                        freq_tracker[max_split[1]*b+unit[1][start_symbol+1]] -= 1
-                        freq_tracker = freq_insert(freq_tracker,unit[1][start_symbol],unit[1][start_symbol+1],b)
-                    
+                        freq_tracker[max_split[1] * b + unit[1][start_symbol + 1]] -= 1
+                        freq_tracker = freq_insert(
+                            freq_tracker,
+                            unit[1][start_symbol],
+                            unit[1][start_symbol + 1],
+                            b,
+                        )
+
         symbol_index.append(sym)
-        freq_tracker[max_split[0]*b+max_split[1]] = 0
+        freq_tracker[max_split[0] * b + max_split[1]] = 0
 
     for unit in n_algs:
-        unit[1] = ''.join([symbol_index[unit[1][x]] for x in range(len(unit[1]))])
+        unit[1] = "".join([symbol_index[unit[1][x]] for x in range(len(unit[1]))])
     return n_algs, compression
 
-def freq_insert(freq_tracker,fb1,fb0,b):
-    duo_index = fb1*b + fb0
+
+def freq_insert(freq_tracker, fb1, fb0, b):
+    duo_index = fb1 * b + fb0
     while len(freq_tracker) <= duo_index:
         freq_tracker.append(0)
     freq_tracker[duo_index] += 1
     return freq_tracker
+
 
 """
 # simple case for early testing
@@ -144,88 +165,99 @@ n_prime_powers = [
 # medium case
 n = 9360
 n_prime_powers = [
-    
+
     [13,7,"L2 U F' R2 F U' L2 U F' R2 F U' L D2 R U F U R' F D' L' F' D' L ",2],
     [5,4,"U2 L F' D R F D R2 D2 L' U' F' U ",2],
     [9,6,"R' U R2 D L' B2 L D' R2 U' R U' ",2],
     [16,4,"U' L2 F2 B L B' U' L' F2 L' F' L' ",3],
     [16,2,"U' F B' R U' B R' U F' B L' U B' L ",7]
-    
+
 ]
 """
 
 # maximum case
 n = 2424240
 n_prime_powers = [
-    [37,17,"U2 L F' R B L2 D2 B B R' F L B2 R' F D' L' F' D' L2 D2 R U F U ",2],
-    [13,12,"L2 U F' R2 F U' L2 U F' R2 F U' L D2 R U F U R' F D' L' F' D' L ",2],
-    [7,6,"R' B2 R U' F2 U R' B2 R U' F2 U R U R U' R2 U2 R L F R' F' L' U ",3],
-    [5,4,"U2 L F' D R F D R2 D2 L' U' F' U ",2],
-    [9,6,"R' U R2 D L' B2 L D' R2 U' R U' ",2],
-    [16,4,"U' L2 F2 B L B' U' L' F2 L' F' L'  ",3],
-    [16,2,"U' F B' R U' B R' U F' B L' U B' L ",7]
+    [37, 17, "U2 L F' R B L2 D2 B B R' F L B2 R' F D' L' F' D' L2 D2 R U F U ", 2],
+    [13, 12, "L2 U F' R2 F U' L2 U F' R2 F U' L D2 R U F U R' F D' L' F' D' L ", 2],
+    [7, 6, "R' B2 R U' F2 U R' B2 R U' F2 U R U R U' R2 U2 R L F R' F' L' U ", 3],
+    [5, 4, "U2 L F' D R F D R2 D2 L' U' F' U ", 2],
+    [9, 6, "R' U R2 D L' B2 L D' R2 U' R U' ", 2],
+    [16, 4, "U' L2 F2 B L B' U' L' F2 L' F' L' ", 3],
+    [16, 2, "U' F B' R U' B R' U F' B L' U B' L ", 7],
 ]
 
 
-replacer = [[" ",""],["R'",'r'],["L'","l"],["U'","u"],["F'","f"],["D'","d"],["B'","b"],["F2","FF"],["U2","UU"],["R2","RR"],["L2","LL"],["B2","BB"],["D2","DD"]]
-symbol_index = ['R','r','L','l','U','u','D','d','F','f','B','b']
-new_symbols = ['A','a','C','c','E','e','G','g','H','h','I','i','J','j','K','k','M','m','N','n','O','o','P','p','Q','q'
-               ,'S','s','T','t','V','v','W','w','X','x','Y','y','Z','z'
-               ,'0','1','2','3','4','5','6','7','8','9'
-               ,'!','@','#','$','%','^','&','*','(',')','-','_','=','+']
-#new_symbols = ['A','a']
+replacer = [
+    [" ", ""],
+    ["R'", "r"],
+    ["L'", "l"],
+    ["U'", "u"],
+    ["F'", "f"],
+    ["D'", "d"],
+    ["B'", "b"],
+    ["F2", "FF"],
+    ["U2", "UU"],
+    ["R2", "RR"],
+    ["L2", "LL"],
+    ["B2", "BB"],
+    ["D2", "DD"],
+]
+symbol_index = ["R", "r", "L", "l", "U", "u", "D", "d", "F", "f", "B", "b"]
+new_symbols = ["A", "a", "C", "c", "E", "e", "G", "g", "H", "h", "I", "i", "J",
+               "j", "K", "k", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q",
+               "S", "s", "T", "t", "V", "v", "W", "w", "X", "x", "Y", "y", "Z",
+               "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@",
+               "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]  # fmt: skip
+# new_symbols = ['A','a']
 
 n_index = 0
 n_algs = []
 curr_unit = 1
 
-#STEP 1: MAKE BASE ALGS
-if True:
-
-    n_prime_powers = add_multipliers(n,n_prime_powers)
+# STEP 1: MAKE BASE ALGS
+if False:
+    n_prime_powers = add_multipliers(n, n_prime_powers)
 
     step_1_out = []
 
     for i, n_pow in enumerate(n_prime_powers):
         for j, n_pow_alg in enumerate(n_pow[5]):
-            step_1_out.append([i,j,n_pow_alg])
+            step_1_out.append([i, j, n_pow_alg])
 
-    file_output(step_1_out,'rubiks_mod_n_base_algs.csv')
+    file_output(step_1_out, "rubiks_mod_n_base_algs.csv")
 
 
-#STEP 2: OPTIMIZE BASE ALGS 
+# STEP 2: OPTIMIZE BASE ALGS
 
 # optimize the algs in rubiks_mod_n_base_algs.csv
 
 
-#STEP 3: MAKE ALL UNIT ALGS
+# STEP 3: MAKE ALL UNIT ALGS
 if False:
+    n_prime_powers = add_multipliers(n, n_prime_powers)
 
-    n_prime_powers = add_multipliers(n,n_prime_powers)
-
-    step_3_in = file_input('rubiks_mod_n_base_algs.csv')
+    step_3_in = file_input("rubiks_mod_n_base_algs.csv")
 
     for alg_in in step_3_in:
         n_prime_powers[int(alg_in[0])][5][int(alg_in[1])] = alg_in[2]
 
-    n_algs = insert_prime_powers(n,0,n_prime_powers,'',[],1)
+    n_algs = insert_prime_powers(n, 0, n_prime_powers, "", [], 1)
 
-    file_output(n_algs,'rubiks_mod_n_unit_algs.csv')
+    file_output(n_algs, "rubiks_mod_n_unit_algs.csv")
 
 
-#STEP 4: OPTIMIZE UNIT ALGS
+# STEP 4: OPTIMIZE UNIT ALGS
 
 # optimize the algs in rubiks_mod_n_unit_algs.csv
 
-#STEP 5: OPTIMIZE UNIT ALGS
-if False:
-
-    step_5_in = file_input('rubiks_mod_n_unit_algs.csv')
+# STEP 5: OPTIMIZE UNIT ALGS
+if True:
+    step_5_in = file_input("rubiks_mod_n_unit_algs.csv")
     for unit in n_algs:
         for rep in replacer:
-            unit[1] = unit[1].replace(rep[0],rep[1])
+            unit[1] = unit[1].replace(rep[0], rep[1])
 
-    n_algs, compression = compress(n_algs,symbol_index,new_symbols)
-
-    file_output(n_algs,'n_alg.csv')
-    file_output(compression,'n_alg_compress.csv')
+    n_algs, compression = compress(n_algs, symbol_index, new_symbols)
+    file_output(n_algs, "n_alg.csv")
+    file_output(compression, "n_alg_compress.csv")
